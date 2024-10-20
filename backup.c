54b8588871ade7d0e5b8f9b2d19a5f3a55c91258
JoarTactic <joar.romcer@gmail.com>
2024-10-20 09:12:25 -0600

    Asignación de memoria dinamica para almacenar ruta absoluta

--- backup.c
+++ backup.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> 
 #include <unistd.h>
 #include <sys/wait.h>
 
+int main(){
+    //Directorio a respaldar
+    char *docsPath = "./MisDocumentos";  // Ruta relativa
+    char *real_docsPath; //Variable donde se almacenará ruta absoluta
+    //Directorio donde se almacenará el respaldo
+    char *backupPath;
+    char *real_backupPath;
+    //Las variables anteriores deberán ser asignadas con los argumentos de main que se introduzcan al ejecutar el programa
+
+    //=================================================================================
+    // Ejemplo de como asignar memoria dinámicamente para la ruta absoluta
+    char *path = "./backup.c";
+    char *real_path;
+    real_path = realpath(path, NULL);  // realpath() asigna memoria si el segundo argumento es NULL
+
+    if (real_path != NULL) {
+        printf("La ruta absoluta es: %s\n", real_path);
+        free(real_path);  // Libera la memoria asignada por realpath
+    } else {
+        perror("Error obteniendo la ruta absoluta");
+    }
+    //=================================================================================
+
+
+    free(real_docsPath);
+    free(real_backupPath);
+    return 0;
+
+}
